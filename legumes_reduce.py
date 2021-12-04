ecaa24479a39e12a56b1870709841dd11cadc9c5
eduardogrilo <jose_egrl@hotmail.com>
2021-12-04 15:19:48 +0000

    Added legumes.py and legumes_reduce.py

---
+++ legumes_reduce.py
@@ -0,0 +1,16 @@
+from functools import reduce
+
+def legumes(lista):
+    lista_invertida = lista[::-1]
+    print('{', end='')
+
+    for i in range (0, len(lista_invertida)):
+        print(' %s' % (lista_invertida[i]), end = ": {")
+    for i in range (0, len(lista_invertida)+1):
+        print('}', end=' ')
+
+
+if __name__ == "__main__":
+    lista1 = ['batatas', 'cenouras', 'tomates', 'favas', 'pepinos', 'cebolas','beterrabas','alhos','couve']
+    print(*lista1, sep=", ")
+    legumes(lista1)
